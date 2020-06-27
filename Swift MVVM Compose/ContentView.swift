7fd5d76c5e3c7b537df113eebe3951eb034c389d
Jesus Luongo <jesus.luongo.lizana@gmail.com>
2020-06-27 08:54:32 -0400

    Initial Commit

---
+++ Swift MVVM Compose/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift MVVM Compose
+//
+//  Created by Jesus Luongo Lizana on 6/27/20.
+//  Copyright © 2020 Jesus Luongo Lizana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
